e7bf8dbf86dcd6474d0ec7439666e213037e006a
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2022-10-25 15:30:17 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -2,13 +2,37 @@ const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const mobile = document.getElementById('mobile-menu')
 const counters = document.querySelectorAll('.counter')
+const li1 = document.querySelector('.li1')
+const li2 = document.querySelector('.li2')
+const li3 = document.querySelector('.li3')
+const li4 = document.querySelector('.li4')
+const li5 = document.querySelector('.li5')
+const li6 = document.querySelector('.li6')
+const li7 = document.querySelector('.li7')
+const li8 = document.querySelector('.li8')
+const li9 = document.querySelector('.li9')
+const li10 = document.querySelector('.li10')
+const li11 = document.querySelector('.li11')
+const li12 = document.querySelector('.li12')
 let scrollStarted = false
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     overlay.classList.toggle('overlay-show')
     document.body.classList.toggle('stop-scrolling')
-    mobile.classList.toggle('hamburger-open')
+    mobile.classList.toggle('menu-open')
+    li1.classList.toggle('li-1')
+    li2.classList.toggle('li-2')
+    li3.classList.toggle('li-3')
+    li4.classList.toggle('li-4')
+    li5.classList.toggle('li-5')
+    li6.classList.toggle('li-6')
+    li7.classList.toggle('li-7')
+    li8.classList.toggle('li-8')
+    li9.classList.toggle('li-9')
+    li10.classList.toggle('li-10')
+    li11.classList.toggle('li-11')
+    li12.classList.toggle('li-12')
 });
 
 document.addEventListener('scroll', scrollPage)
